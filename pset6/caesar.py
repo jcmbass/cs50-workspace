21468782cfdeb282c83f86b8527577a6af4c3c70
jcmbass <jcmbass@gmail.com>
2017-02-10 03:01:52 +0000

    caesar.py implementation

--- pset6/caesar.py
+++ pset6/caesar.py
@@ -1,12 +1,26 @@
 import cs50
+import sys
 
-def main():
-    if self.arg != 2:
-	    print("YELL")
-    
-    else:
-        k = cs50.atoi(arg)
-        s = cs50.get_string()
+
+if len(sys.argv) != 2:
+    print("missing command-line argument")
+    exit(1)
+
+else:
+    print("s: ", end="")
+    s = cs50.get_string()
+    if s == None:
+        exit(1)
         
-if __name__=="__main__":
-    main()
+    for i in range(len(s)):
+        if(s[i].isalpha()):
+            if(s[i].isupper()):
+                c = chr(((abs(ord('A') - ord(s[i])) + int(sys.argv[1])) % 26) + ord('A'))
+                print(c, end="")
+            elif(s[i].islower()):
+                c = chr(((abs(ord('a') - ord(s[i])) + int(sys.argv[1])) % 26) + ord('a'))
+                print(c, end="")
+            else:
+                print("{}".format(s[i]))
+    print()
+    exit(0)
